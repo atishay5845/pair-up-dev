3719ac1da95de41d73ce07f3eedc5282abe88774
atishay5845 <atishaysodhiya5845@gmail.com>
2026-05-04 23:50:18 +0530

    completed 1st part

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use("/test",(req,res)=>{
-  res.send('Hello From server');
+  res.send('Hello From server ..');
 })
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
