4de9a4d1398cdf5d34d03a39670db9feca51fb05
atishay5845 <atishaysodhiya5845@gmail.com>
2026-05-09 22:20:15 +0530

    using next function while returning first function

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const app = express();
 app.get("/user",
   (req,res,next)=>{
   console.log("First callback");
-  // res.send({firstname:"Atishay",lastname:"Sodhiya"});
-  next();
+  res.send({firstname:"Atishay",lastname:"Sodhiya"});
+  next();//now it will go to the second callback and send "Second callback" as response, but it will not work because we have already sent a response in the first callback. So we will get an error "Error: Can't set headers after they are sent."
   }
   ,(req,res)=>{
     console.log("Second callback");
