917733cb17106a1f41a2c86b5d5850963fb77117
atishay5845 <atishaysodhiya5845@gmail.com>
2026-05-13 23:51:26 +0530

    implemented dummy middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 
+app.get("/admin/getalldata", (req,res)=>{
+  //logic to check if the req is auth 
+  const token ="xyz";
+  const isAdminAuthorized = token === "xyz"; //this is just a dummy check, in real scenario we will check the token from the database or some other source
 
+  if(isAdminAuthorized){
+    res.send("all data sent");
+  }else{
+    res.status(403).send("Unauthorized");
+  }
+});
+
+app.get("/admin/deleteuser", (req,res)=>{
+  res.send("delete user");
+});
 app.get("/user",
   (req,res,next)=>{
   console.log("First callback");
   res.send({firstname:"Atishay",lastname:"Sodhiya"});
-  next();//now it will go to the second callback and send "Second callback" as response, but it will not work because we have already sent a response in the first callback. So we will get an error "Error: Can't set headers after they are sent."
+  // next();//now it will go to the second callback and send "Second callback" as response, but it will not work because we have already sent a response in the first callback. So we will get an error "Error: Can't set headers after they are sent."
+
   }
   ,(req,res)=>{
     console.log("Second callback");
