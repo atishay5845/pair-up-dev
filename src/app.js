707bcdf9d73a29546edffc84386fe86c9ae580dd
atishay5845 <atishaysodhiya5845@gmail.com>
2026-05-15 12:58:02 +0530

    trying error

--- src/app.js
+++ src/app.js
@@ -3,34 +3,47 @@ const app = express();
 
 const {adminAuth} = require("./middlewares/auth");
 
-app.get("/admin/dashboard", adminAuth, (req,res)=>{
-    res.send("Welcome to the admin dashboard");
+app.get("/", (req, res, next) => {
+    try {
+        // some code
+        res.send("Hello World");
+    } catch (err) {
+        next(err);
+    }
 });
 
-app.get("/admin/getalldata", adminAuth, (req,res)=>{
-    res.send("all data sent");
+app.use((err, req, res, next) => {
+    res.status(500).send("Internal Server Error");
 });
 
-app.get("/admin/deleteuser", adminAuth, (req,res)=>{
-  res.send("delete user");
-});
+// app.get("/admin/dashboard", adminAuth, (req,res)=>{
+//     res.send("Welcome to the admin dashboard");
+// });
 
-//works for /abc and /ac  
-app.get(/ab?c/, (req,res)=>{
-  res.send("ab?c");
-});
+// app.get("/admin/getalldata", adminAuth, (req,res)=>{
+//     res.send("all data sent");
+// });
 
-//works for /abc, /abbc, /abbbc and so on
-app.get(/ab+c/, (req,res)=>{
-  res.send("ab+c");
-});
+// app.get("/admin/deleteuser", adminAuth, (req,res)=>{
+//   res.send("delete user");
+// });
 
-app.get(/ab*c/, (req,res)=>{//works for /ac, /abc, /abbc, /abbbc and so on
-  res.send("ab*c");
-});
-app.get(/a.c/, (req,res)=>{//works for /abc, /a-c, /a c and so on
-  res.send("a.c");
-});
+// //works for /abc and /ac  
+// app.get(/ab?c/, (req,res)=>{
+//   res.send("ab?c");
+// });
+
+// //works for /abc, /abbc, /abbbc and so on
+// app.get(/ab+c/, (req,res)=>{
+//   res.send("ab+c");
+// });
+
+// app.get(/ab*c/, (req,res)=>{//works for /ac, /abc, /abbc, /abbbc and so on
+//   res.send("ab*c");
+// });
+// app.get(/a.c/, (req,res)=>{//works for /abc, /a-c, /a c and so on
+//   res.send("a.c");
+// });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
