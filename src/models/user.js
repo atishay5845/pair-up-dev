045694c23b66eec459c375ac3c0ff8b4728a79d6
atishay5845 <atishaysodhiya5845@gmail.com>
2026-05-18 00:26:39 +0530

    created user schema model

---
+++ src/models/user.js
@@ -0,0 +1,30 @@
+const moongoose = require("mongoose");
+
+const userSchema = new moongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age:{
+    type: Number,
+    required: true,
+  }
+})
+
+//create mongoose model
+
+const User = moongoose.model("User", userSchema);
+module.exports = User;
